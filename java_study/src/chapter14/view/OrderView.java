0b0f761fb926fb05216e7231b1179cd601bf9fd2
gameovermin <qorkrhvms@gmail.com>
2024-09-10 16:58:57 +0900

    tester

--- java_study/src/chapter14/view/OrderView.java
+++ java_study/src/chapter14/view/OrderView.java
@@ -1,5 +1,22 @@
 package chapter14.view;
 
-public class OrderView {
+import java.util.Scanner;
 
+public class OrderView {
+	//	사용자로부터 주문을 받는 로직
+	private Scanner sc = new Scanner(System.in);
+	
+	// 메뉴 선택을 사용자에게 요처하는 메서드
+	public String getUserChoice()  {
+		System.err.println("==================");
+		System.out.println("Select an option : ");
+		System.out.println("1. Pizza");
+		System.out.println("2. Pasta");
+		System.out.println("3. Coke");
+		System.out.println("4. Stake");
+		System.out.println("Your Choice : ");
+		return sc.nextLine();
+	}
+	
+	
 }
